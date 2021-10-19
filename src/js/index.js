8bf616ec8af34cdf3e5597102c3ea51c11b7cddf
sarahbejerano <bejeranosarah@gmail.com>
2021-10-19 20:39:19 +0000

    wip exercise

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,31 @@
 //import react into the bundle
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
-
-//include bootstrap npm library into the bundle
 import "bootstrap";
-
-//include your index.scss file into the bundle
 import "../styles/index.scss";
 
-//import your own components
-import Home from "./component/home.jsx";
+// por que el contador cuenta 10 segundos??
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+function SimpleCounter() {
+	const [counter, setCounter] = useState(0);
+	useEffect(function() {
+		const intervalId = setInterval(function() {
+			setCounter(counter => counter + 1);
+			console.log(counter);
+		}, 1000); //1000 es un segundo
+		return function() {
+			clearInterval(intervalId);
+		};
+	}, []);
+	return (
+		<div className="bigCounter">
+			<div className="clock">I</div>
+			<div className="four">{Math.floor(counter / 10000) % 10}</div>
+			<div className="three">{Math.floor(counter / 1000) % 10}</div>
+			<div className="two">{Math.floor(counter / 100) % 10}</div>
+			<div className="one">{Math.floor(counter / 10) % 10}</div>
+		</div>
+	);
+}
+
+ReactDOM.render(<SimpleCounter />, document.querySelector("#app"));
